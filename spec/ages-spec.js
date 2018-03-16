ed3b4d543343bf1118e3bc92e08bd54961063548
Human Flesh Body World <hfbw@Humans-iMac.domain>
2018-03-16 14:40:30 -0700

    create red test condition for remaining years on jupiter test method

--- spec/ages-spec.js
+++ spec/ages-spec.js
@@ -90,7 +90,7 @@ describe('Age', function(){
     let jupiterAge = userAge.marsAge;
     console.log(jupiterAge);
     //assert
-    expect(jupiterAge).toBeLessThan(-100);
+    expect(jupiterAge).toBeLessThan(-1000);
 
   });
 
